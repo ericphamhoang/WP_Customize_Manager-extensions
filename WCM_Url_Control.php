d29cf207467ddad6c79898de91be4ffbdb4a0360
Phương Phạm Hoàng <ericphamhoang@gmail.com>
2017-01-06 10:05:38 +1100

    Add Url Control

---
+++ WCM_Url_Control.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Owlies;
+/**
+ * Created by PhpStorm.
+ * User: nikon_000
+ * Date: 1/4/2017
+ * Time: 2:42 PM
+ */
+class WCM_Url_Control extends WCM_Control
+{
+
+    public function __construct($title, $section)
+    {
+        parent::__construct($title, $section);
+    }
+
+    public function render(\WP_Customize_Manager $wp_customize)
+    {
+        $wp_customize->add_setting($this->id . '__settings', array(
+            'default' => '',
+            'transport' => 'refresh',
+        ));
+
+        $wp_customize->add_control(new \WP_Customize_Control($wp_customize,
+            $this->id,
+            array(
+                'label' => $this->title,
+                'section' => $this->section,
+                'settings' => $this->id . '__settings',
+                'type' => 'url'
+            )));
+    }
+}
--- autoload.php
+++ autoload.php
@@ -2,8 +2,9 @@
 
 include_once __DIR__.'/WCM_Control.php';
 include_once __DIR__.'/WCM_Image_Control.php';
+include_once __DIR__.'/WCM_Url_Control.php';
 include_once __DIR__.'/WCM_Number_Control.php';
 include_once __DIR__.'/WCM_Text_Control.php';
 include_once __DIR__.'/WCM_Textarea_Control.php';
 include_once __DIR__.'/WCM_Section.php';
-include_once __DIR__.'/WCM_Utils.php';
+include_once __DIR__.'/WCM_Utils.php'; 
